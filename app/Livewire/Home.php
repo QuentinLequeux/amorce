2e8eca8231729a6d2e4f55512d47e933f1fc6d51
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-08-05 21:48:59 +0200

    Add widgets on dashboard

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,13 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Fund;
 use Livewire\Component;
+use App\Models\Transaction;
 
 class Home extends Component
 {
     public function render()
     {
         $user = auth()->user();
-        return view('livewire.home', compact('user'));
+        $funds = Fund::all();
+        foreach ($funds as $fund) {
+            $fund->total = Transaction::getTotalByFundId($fund->id);
+        }
+        return view('livewire.home', compact('user', 'funds'));
     }
 }
---
+++ resources/views/components/widget.blade.php
@@ -0,0 +1,23 @@
+<div class="flex flex-wrap gap-4">
+    @foreach($funds as $fund)
+        <div class="mt-10 p-6 w-[300px] h-auto shadow-lg rounded-2xl flex items-center dark:bg-[#000]">
+            <div class="px-4">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+                     stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                     stroke-linejoin="round" class="lucide lucide-landmark-icon lucide-landmark">
+                    <path d="M10 18v-7"/>
+                    <path
+                        d="M11.12 2.198a2 2 0 0 1 1.76.006l7.866 3.847c.476.233.31.949-.22.949H3.474c-.53 0-.695-.716-.22-.949z"/>
+                    <path d="M14 18v-7"/>
+                    <path d="M18 18v-7"/>
+                    <path d="M3 22h18"/>
+                    <path d="M6 18v-7"/>
+                </svg>
+            </div>
+            <div>
+                <p class="font-bold">{{ $fund->total / 100 }}<span class="ml-1">€</span></p>
+                <p>{{ $fund->name }}</p>
+            </div>
+        </div>
+    @endforeach
+</div>
